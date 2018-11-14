329bb5390a94685f66d5167f292a04046dc2af3d
Yuliia Pavlyk <ypavly@softserveinc.com>
2018-11-14 11:59:10 +0200

    added logic to transfer service

--- my-app/src/app/main-layout/favourite-document/favourite-document.component.ts
+++ my-app/src/app/main-layout/favourite-document/favourite-document.component.ts
@@ -1,6 +1,7 @@
-import { Component, OnInit } from '@angular/core';
-import { MatTableDataSource } from '@angular/material';
-import { Subject, Subscription } from 'rxjs';
+import { Component, OnInit,ViewChild } from '@angular/core';
+import { MatTableDataSource , MatTable} from '@angular/material';
+import {  Subscription } from 'rxjs';
+import { take } from 'rxjs/operators';
 
 import { FavouriteDocumentService } from '../../main-layout/services/favourite-document.service';
 import { FavouriteDocument } from '../../shared/models';
@@ -18,35 +19,71 @@ export class FavouriteDocumentComponent implements OnInit {
   idUser: number;
   document: Document;
   favouriteDoc: FavouriteDocument;
-  //data:boolean=false;
+  subscription: Subscription;
   interval: Subscription;
-  dataSource = new MatTableDataSource<Document[]>(this.documents);
+  loading: boolean = true;
+  dataSource = new MatTableDataSource<any>();
   displayedColumns: string[] = ['Favourite', 'Name', 'Description', 'Author', 'CreateDate', 'ModifiedDate'];
-  destroy$: Subject<boolean> = new Subject<boolean>();
+  @ViewChild(MatTable) mytable: MatTable<any>;
 
   constructor(private favouriteDocumentService: FavouriteDocumentService,
     private transferService: TransferService
   ) { }
 
-  deleteFromFavourite(idDocument: number, idRow: number, row) {
+  deleteFromFavourite(idDocument: number, idRow: number, document) {
     let favouriteDoc = { UserId: 1, DocumentId: idDocument };
     this.favouriteDocumentService.deleteFromFavouriteDocuments(favouriteDoc as FavouriteDocument).subscribe(res => {
       //  this.loadFavourites();
     });
+    this.transferService.removeDocumentFromFavourite(document);
+
   }
 
   loadFavourites(): void {
     this.favouriteDocumentService.getFavouriteDocuments().subscribe(res => {
-      this.documents = res;
-      this.dataSource.data = this.documents;
+      //this.documents = res;
+      //this.dataSource.data = this.documents;
+
     });
   }
 
   ngOnInit() {
-    this.documents = this.transferService.getFavourite();
-    this.dataSource.data = this.documents;
 
+    
+    this.transferService.getFavourite();
+    this.transferService.documentInitializing
+      .pipe(take(1))
+      .subscribe(res => {
+        debugger
+        //this.documents = res;
+        //this.dataSource.data = this.documents;
+        //this.dataSource = new MatTableDataSource(res);
+        console.log(res);
+        this.dataSource = new MatTableDataSource(res);
+      //  this.documents= res;
+     //   this.dataSource.data=this.documents;
+       //  = new MatTableDataSource();
+       // this.dataSource.data = res;
+      // console.log( this.dataSource.data + "data in data source");
+       
+        this.mytable.renderRows();
+      })
+
+    this.transferService.documentChanges.subscribe(res => {
+      if (this.documents.indexOf(document)!=-1) {
+        console.log("push");
+        this.documents.push(res);
+        console.log(this.documents);
+       // this.dataSource.data = this.documents;
+        
+      }
+      else {
+        console.log("delete");
+        this.documents.splice(this.documents.indexOf(document), 1);
+      }
+    })
   }
 
-}
 
+
+}
--- my-app/src/app/main-layout/services/transfer.service.ts
+++ my-app/src/app/main-layout/services/transfer.service.ts
@@ -1,33 +1,58 @@
 import { Injectable } from '@angular/core';
-import { Subject } from 'rxjs';
+import { Subject,Observable, of} from 'rxjs';
+
 
 import { FavouriteDocumentService } from './favourite-document.service'
-import { Document } from '../../shared/models';
 
-@Injectable()
+import { Document } from '../../shared/models/document';
+
+@Injectable({
+  providedIn: 'root'
+})
 export class TransferService {
 
   constructor(
     private favouriteDocumentService: FavouriteDocumentService
   ) { }
-  documents: Document[];
+  documents: Document[] ;
+  loading: boolean = true;
 
-  public addDocs = new Subject<Document>();
+  public loading$ = new Subject<Document[]>();
+  documentInitializing = this.loading$.asObservable();
+
+  public documentStream$ = new Subject<Document>();
+  documentChanges = this.documentStream$.asObservable();
+
+  addToStream(document: Document): void {
+    this.documentStream$.next(document);
+  }
+
+
 
   addDocumentFromFavourite(document: Document): void {
     this.documents.push(document);
   }
 
   removeDocumentFromFavourite(document: Document): void {
+    console.log(document);
     this.documents.splice(this.documents.indexOf(document), 1);
   }
 
-  getFavourite(): Document[] {
+  getFavourite(): Observable<Document[]> {   
+    
     if (!this.documents) {
-      this.favouriteDocumentService.getFavouriteDocuments().subscribe(res => {
-        this.documents = res;
-      });
-      return this.documents;
+      this.favouriteDocumentService.getFavouriteDocuments().subscribe(result => {
+        this.loading$.next(result);
+        this.documents = result;
+        return this.documents;
+      })
+    }
+    else {
+      debugger
+      this.loading$.next(this.documents);
+      return of(this.documents);
     }
   }
 }
+
+
